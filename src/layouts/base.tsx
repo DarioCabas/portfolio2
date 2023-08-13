bc5caeacb101c1a37948794694093d8ad6dd976e
DarioCabas <hz-hertzio@hotmail.com>
2023-08-12 20:33:48 -0500

    add favicon

--- src/layouts/base.tsx
+++ src/layouts/base.tsx
@@ -7,14 +7,17 @@ interface BaseLayoutProps {
   children: ReactNode | ReactNode[];
 }
 
-const BaseLayout: React.FC<BaseLayoutProps> = ({ children })=> {
+const BaseLayout: React.FC<BaseLayoutProps> = ({ children }) => {
   return (
     <div id="main">
       <Head>
         <title>Darío Cabascango | Web & Mobile developer</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.json" />
-        
+        <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+        <link rel="manifest" href="/images/site.webmanifest" />
+
         <link
           href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;800&display=swap"
           rel="stylesheet"
@@ -23,7 +26,7 @@ const BaseLayout: React.FC<BaseLayoutProps> = ({ children })=> {
           href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap"
           rel="stylesheet"
         />
-        
+
       </Head>
       {children}
     </div>
