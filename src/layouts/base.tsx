5a2b4d5b8ccfef8c6aa5d61338d51703a27cf643
DarioCabas <hz-hertzio@hotmail.com>
2023-08-12 20:21:06 -0500

    add opacity

--- src/layouts/base.tsx
+++ src/layouts/base.tsx
@@ -14,12 +14,7 @@ const BaseLayout: React.FC<BaseLayoutProps> = ({ children })=> {
         <title>Darío Cabascango | Web & Mobile developer</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#181818" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="theme-color" content="#ffffff" />
+        
         <link
           href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;800&display=swap"
           rel="stylesheet"
@@ -28,28 +23,7 @@ const BaseLayout: React.FC<BaseLayoutProps> = ({ children })=> {
           href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap"
           rel="stylesheet"
         />
-        <meta
-          name="viewport"
-          key="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@dario_cabas93" />
-        <meta property="og:site_name" content="Darío Cabascango  | Web & Mobile developer" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Darío Cabascango | Web & Mobile developer" />
-        <meta property="og:locale" content="en" />
-        <meta property="og:url" content="https://ayaumadev.com/" />
-        <meta
-          name="description"
-          content="Hello! a Software Developer based in Quito, Ecuador. I enjoy creating beautiful and reliable applications for internet and phones. My goal is to always build scalable products and performant experiences."
-        />
-        <meta
-          name="keywords"
-          content="Developer, Javascript, Freelancer, React, React native, React Developer"
-        />
-        <meta property="og:image" content="" />
-        <meta property="twitter:image" content="" />
+        
       </Head>
       {children}
     </div>
